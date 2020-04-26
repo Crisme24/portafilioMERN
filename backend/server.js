09b554d0cb2720c1a4cb35be19f511fcd87237d8
Crisme24 <crisme-24@hotmail.com>
2020-04-26 10:56:10 +0100

    first commit

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const connectDB = require('./config/db');
+
+const app = express();
+
+//Connect Database
+connectDB();
+
+//init middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
+app.get('/', (req, res) => res.send('API Running'));
+
+//Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
