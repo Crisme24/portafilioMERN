7953dc735c1ed0c6dd8a469a76e0159824b34abb
Crisme24 <crisme-24@hotmail.com>
2020-04-27 19:59:48 +0100

    second commit

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -51,7 +51,7 @@ router.post('/', [
         const salt = await bcrypt.genSalt(10);
         user.password = await bcrypt.hash(password, salt);
 
-        await user.save();
+        await user.replaceOne();
 
         //Encrypt password
         const payload = {
