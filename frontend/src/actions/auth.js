d27fe54c7693e0449457f8fe4a3499a2b6fc7b32
Crisme24 <crisme-24@hotmail.com>
2020-04-29 23:19:02 +0100

    view of create profile

--- frontend/src/actions/auth.js
+++ frontend/src/actions/auth.js
@@ -7,7 +7,8 @@ import {
     AUTH_ERROR,
     LOGIN_SUCCESS,
     LOGIN_FAIL,
-    LOGOUT
+    LOGOUT,
+    CLEAR_PROFILE
 } from './types';
 import setAuthToken from '../utils/setAuthToken';
 
@@ -25,7 +26,7 @@ export const loadUser = () => async dispatch => {
             payload: res.data
         });
 
-        dispatch(loadUser());
+        //dispatch(loadUser());
     } catch (err) {
         dispatch({
             type: AUTH_ERROR
@@ -51,7 +52,7 @@ export const register = ({ name, email, password }) => async dispatch => {
             payload: res.data
         });
 
-        dispatch(loadUser());
+        //dispatch(loadUser());
 
     } catch (err) {
         const errors = err.response.data.errors;
@@ -99,5 +100,6 @@ export const login = (email, password) => async dispatch => {
 
 //logout / Clear Profile
 export const logout = () => dispatch => {
+    dispatch({ type: CLEAR_PROFILE });
     dispatch({ type: LOGOUT });
 };
